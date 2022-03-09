a929544a5b3914e990b5cb0a072d75810ca4fb8a
陈金栋 <31403062+Jackywine@users.noreply.github.com>
2022-03-09 14:47:54 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'cee56472b76c41d8bc75cd1b6446ffea',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jackywine’s Blog',
+  domain: 'https://illustrious-heron-c0c.notion.site/Jackywine-s-Blog-cee56472b76c41d8bc75cd1b6446ffea',
+  author: 'Jackywine',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'https://twitter.com/Jackywine1998',
+  Behance: 'https://www.behance.net/chenjindong',
+  Instagram: 'https://www.instagram.com/jackycjd/',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
